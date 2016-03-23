56b9a14d9511d59f36d1f819f90c66799045f6c2
Ben Miller <benjaminmiller229@gmail.com>
2016-03-23 13:53:40 -0400

    Update dec_to_bin.py

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,8 +1,10 @@
 def decimalToBinary(n):
+    # base cases
     if(n==0):
         return "0"
     elif(n==1):
         return "1"
+    #increment throught the number by dividing by integer 2, and return it's parity (n%2) as 1 or 0
     else:
         return decimalToBinary(n//2) + "" +  decimalToBinary(n%2)
 
